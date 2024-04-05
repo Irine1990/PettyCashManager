17189844d7e56de1c65f27a19023feea17c4fefe
Irine1990 <Irine1990@gmail.com>
2024-04-05 18:28:49 +0530

    first commit

---
+++ src/utils/ApiRequest.js
@@ -0,0 +1,9 @@
+
+
+export const setAvatarAPI = 'http://localhost:8080/api/auth/setAvatar';
+export const registerAPI = 'http://localhost:8080/api/auth/register';
+export const loginAPI = 'http://localhost:8080/login/api/auth/login';
+export const addTransaction = 'http://localhost:8080/api/v1/addTransaction';
+export const getTransactions = 'http://localhost:8080/api/v1/getTransaction';
+export const editTransactions = 'http://localhost:8080/api/v1/updateTransaction';
+export const deleteTransactions = 'http://localhost:8080/api/v1/deleteTransaction';
